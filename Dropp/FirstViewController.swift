ee422ba53ba6d9902e7f552b0d056607afccdff7
Jeffery Jackson, Jr <jefferyjackson.jr@me.com>
2018-07-04 17:33:46 -0500

    Initial Commit

---
+++ Dropp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dropp
+//
+//  Created by Jeffery Jackson, Jr. on 7/4/18.
+//  Copyright © 2018 Thumb Taps, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
